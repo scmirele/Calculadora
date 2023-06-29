1dcfe8c7c34153c686b0b4b244b1c435748572fc
scmirele <mirelecn@gmail.com>
2023-06-28 22:36:37 -0300

    removendo comentários

--- script.js
+++ script.js
@@ -16,28 +16,3 @@ function calculate() {
     document.querySelector('.screen').innerHTML = "nothing..."
   }
 }
-
-
-// function calculate(value) {
-  //     console.log(value)
-  //   switch(operation) {
-    //     case '+':
-    //        return(number1 + number2)
-    //     case '-':
-    //        return(number1 - number2)
-    //      case '*':
-    //        return(number1 * number2)
-    //      case '/':
-    //        return(number1 / number2)
-    //      default:
-    //        return(NaN)
-//   }
-// }
-            
-  //   document.querySelector('.screen').innerHTML = operation + op;  
-  // }
-  // buttons.addEventListener("click")
-  // buttonsValores.addEventListener("click", calculate)
-  
-  // let buttons = document.querySelectorAll('.container-buttons')
-  // let buttonsValores = document.querySelectorAll('.buttons')
